a9d073d043974bc3082e7b8473040191f3d2029a
Asia Kane and Spencer Ruiz <spencer@>
2017-03-29 14:44:11 -0700

    add piglatin function to parse small words

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,6 @@ const ISWORD = function(word) {
   }
 };
 
-// If string is a string, return string, else return string
-const PIGOUTPUT = function(word) {
-  if (ISWORD(word) === true) {
-    return WORDAY(word);
-  } else {
-    return word;
-  }
-};
 
 // Add ay to end of a word
 const WORDAY = function(word) {
@@ -30,7 +22,7 @@ const STRTOARRAY = function(word) {
   return word.split("");
 };
 
-// Return first element of an array
+// Return first element of an array, which is a string
 const FIRSTLETTER = function(word){
   return (STRTOARRAY(word)[0]);
 };
@@ -45,19 +37,48 @@ const ISVOWEL = function(letter) {
 };
 
 // Move first element of array to end of array
-const FIRSTTOEND = function(array){
-  const FIRST = array.shift();
-  array.push(FIRST);
-  return array;
+const FIRSTTOEND = function(letters){
+  const FIRST = letters.shift();
+  letters.push(FIRST);
+  return letters.join("");
 };
 
+const ISSINGLE = function(word) {
+  if (word.length === 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// If string is a string, return string, else return string
+const PIGOUTPUT = function(word) {
+  if (ISWORD(word) === true) { // input is all letters
+    if (ISSINGLE(word) ===  true) { // word is single letter
+      if (ISVOWEL(FIRSTLETTER(word)) === true) { //single letter is a vowel
+        return WORDAY(word); //return single letter with ay
+      } else { // single letter is consonant
+        return word; // do nothing to word, return word
+      }
+    } else { // not a single letter word
+      if (ISVOWEL(FIRSTLETTER(word)) === true) { // multiple letter word with first letter a vowel
+        return WORDAY(word); // return word with ay at the end
+      }
+      else { // word is multi letter and starts with a consonant
+        return WORDAY(FIRSTTOEND(STRTOARRAY(word))); // return word with consonant at end and add ay
+      }
+    }
+  } else {
+    return word;
+  }
+};
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#pig-latin-form").submit(function(event) {
     event.preventDefault();
     let userInput = $("input#pig-latin-input").val();
-    console.log(FIRSTTOEND(STRTOARRAY(userInput)));
+    console.log(PIGOUTPUT(userInput));
   });
 
 
