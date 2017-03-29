f99ee2ef09b9cdd74a439cc36b4ba651110ed69b
Asia Kane and Spencer Ruiz <spencer@>
2017-03-29 11:19:56 -0700

    add js user interface logic and business logic to return iay when user input is i

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
 //Business Logic
 
+// If string is only letters, return true, else false
 const ISWORD = function(word) {
-  word.match(/^[A-Za-z]+$/);
+  if (word.match(/^[A-Za-z]+$/)) {
+    return true ;
+  } else {
+    return false;
+  }
 };
 
-// User Interface Logic 
+// If string is a string, return string, else return string
+const PIGOUTPUT = function(word) {
+  if (ISWORD(word) === true) {
+    return WORDAY(word);
+  } else {
+    return word;
+  }
+};
+
+// If single letter word begins with vowel, add ay to end of word, else do nothing
+const WORDAY = function(word) {
+  return word + "ay";
+};
+
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#pig-latin-form").submit(function(event) {
+    event.preventDefault();
+    let userInput = $("input#pig-latin-input").val();
+    console.log(PIGOUTPUT(userInput));
+  });
+
+
+
+}); // document ready close
