c9d3fbc8ed5569ac680ae25f2e289747f06f93a1
Asia Kane and Spencer Ruiz <asia@>
2017-03-29 12:00:58 -0700

    add business logic to Split string into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 //Business Logic
 
+
+
 // If string is only letters, return true, else false
 const ISWORD = function(word) {
   if (word.match(/^[A-Za-z]+$/)) {
@@ -18,17 +20,35 @@ const PIGOUTPUT = function(word) {
   }
 };
 
-// If single letter word begins with vowel, add ay to end of word, else do nothing
+// Add ay to end of a word
 const WORDAY = function(word) {
   return word + "ay";
 };
 
+// Split string into an array
+const STRTOARRAY = function(word) {
+  return word.split("");
+};
+
+// Evaluate if first element in array is a vowel, return true, else return false
+// const ISFIRSTVOWEL = function(word){
+//   const VOWELS = ["a","e","i","o","u"];
+//   VOWELS.forEach(function(vowel){
+//     let firstLetter = STRTOARRAY(word)[0];
+//     if (firstLetter === vowel) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// };
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#pig-latin-form").submit(function(event) {
     event.preventDefault();
     let userInput = $("input#pig-latin-input").val();
+    console.log(ISFIRSTVOWEL(userInput));
     console.log(PIGOUTPUT(userInput));
   });
 
